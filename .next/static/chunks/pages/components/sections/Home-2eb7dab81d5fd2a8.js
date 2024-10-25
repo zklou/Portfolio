c5545863e771c3ecfc529446662c33812a8ddada
Zhengkun Lou <41825013+zklou@users.noreply.github.com>
2024-10-25 15:34:36 -0400

    build about

--- .next/static/chunks/pages/components/sections/Home-2eb7dab81d5fd2a8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[28],{374:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/sections/Home",function(){return o(7735)}])}},function(n){n.O(0,[296,445,260,627,570,675,281,238,742,522,926,778,462,651,74,735,774,888,179],function(){return n(n.s=374)}),_N_E=n.O()}]);
