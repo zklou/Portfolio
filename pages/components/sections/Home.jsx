8afe4eabd2a99421378ed8c5434f43b7d28cf864
Zhengkun Lou <41825013+zklou@users.noreply.github.com>
2024-10-26 15:33:47 -0400

    upadte style

--- pages/components/sections/Home.jsx
+++ pages/components/sections/Home.jsx
@@ -81,43 +81,42 @@ const Home = () => {
         {/* Desktop Home */}
         <div className="relative border-0 sm1:hidden md:block h-[50rem]">
           <div className="w-4/5 pt-20 m-auto border-0 opacity-1" id="main">
-            <div className="z-0 text-2xl font-normal tracking-tighter text-white border-0 font-change2">
+            <div className="z-0 text-3xl font-light tracking-tighter text-gray-300 border-0 font-change2">
               <div className="absolute right-0 w-2/5 text-5xl border-0 sm1:hidden lg:block">
                 <Lottie animationData={threedSpheres} className="z-0 grayscale opacity-50 invert" loop={true} />
               </div>
-              <motion.span className="sm1:text-4xl md:text-5xl lg:text-6xl absolute lg:top-[15rem] md:top-[15.5rem]">Hello</motion.span>
-              <span className="border-0 absolute top-[16.5rem] xl:left-[18rem] lg:left-[15rem] md:left-[11.5rem]">&nbsp; I&apos;m </span>
-              <span className="text-gray-400 border-0 absolute top-[16.5rem] xl:left-[20.8rem] lg:left-[17.5rem] md:left-[14.2rem]">&nbsp;Zhengkun Lou</span>
+              <motion.span className="sm1:text-3xl md:text-4xl lg:text-5xl absolute lg:top-[15rem] md:top-[15.5rem] text-gray-300">Hello</motion.span>
+              <span className="border-0 absolute top-[16.5rem] xl:left-[18rem] lg:left-[15rem] md:left-[11.5rem] text-gray-300">&nbsp; I&apos;m </span>
+              <span className="text-blue-400 border-0 absolute top-[16.5rem] xl:left-[20.8rem] lg:left-[17.5rem] md:left-[14.2rem]">&nbsp;Zhengkun Lou</span>
             </div>
-            <div className="absolute z-10 font-extrabold tracking-tighter text-white lg:text-8xl xl:text-9xl font-change2 top-[20rem] md:text-7xl sm3:text-7xl">FULLSTACK</div>
-            <div className="z-30 font-extrabold tracking-tighter text-white lg:text-8xl xl:text-9xl font-change2 font-stencil2 top-[20rem] absolute md:text-7xl sm3:text-7xl">FULLSTACK</div>
-            <div className="absolute z-10 font-extrabold tracking-tighter text-white lg:text-8xl xl:text-9xl font-change2 xl:top-[28rem] md:top-[25rem] lg:top-[26rem] md:text-7xl sm3:text-7xl">
-              <span className="text-blue-400/60 font-stencil23">WEB</span>
-              <span className="sm1:block md:inline">&nbsp;DEVELOPER</span>
-            </div>
-            <div className="absolute z-30 font-extrabold tracking-tighter text-white lg:text-8xl xl:text-9xl font-change2 font-stencil2 md:top-[25rem] xl:top-[27.95rem] lg:top-[26rem] md:text-7xl sm3:text-7xl">
-              <span className="text-blue-400/60 font-stencil23">WEB</span>
-              <span className="sm1:block md:inline">&nbsp;DEVELOPER</span>
+
+            <div className="absolute z-10 font-extrabold tracking-tighter text-white lg:text-7xl xl:text-8xl font-change2 top-[20rem]">FULLSTACK</div>
+            <div className="absolute z-10 font-extrabold tracking-tighter text-blue-400/80 lg:text-7xl xl:text-8xl font-change2 xl:top-[28rem]">
+              WEB DEVELOPER
             </div>
           </div>
-          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[24rem] lg:top-[21rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-9xl font-extrabold font-stencil24">
+
+          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[24rem] lg:top-[21rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-8xl font-extrabold font-stencil24">
             design design design design ...
           </motion.div>
-          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[30rem] lg:top-[28rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-9xl font-extrabold font-stencil24">
+          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[30rem] lg:top-[28rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-8xl font-extrabold font-stencil24">
             code code code code code ...
           </motion.div>
-          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[35rem] lg:top-[34.5rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-9xl font-extrabold font-stencil24">
+          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[35rem] lg:top-[34.5rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-8xl font-extrabold font-stencil24">
             develop develop develop ...
           </motion.div>
-          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[42rem] lg:top-[41rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-9xl font-extrabold font-stencil24">
+          <motion.div style={{ x: toRight3 }} className="tracking-tighter md:top-[42rem] lg:top-[41rem] absolute text-gray-400/30 z-0 overflow-hidden whitespace-nowrap text-8xl font-extrabold font-stencil24">
             deploy deploy deploy ...
           </motion.div>
+
           <div className="absolute w-4/5 m-auto border-0 top-[2rem] lg:left-[9rem] xl:left-[10rem] md:left-[5rem] z-20">
             <Image src="./media/images/meformal.png" alt="Zhengkun Lou" width={700} height={100} className="m-auto transition duration-300 transform grayscale brightness-75" />
           </div>
+
           <div className="absolute z-40 md:w-1/3 lg:w-1/4 xl:w-1/5 text-sm text-gray-300 md:left-[5rem] md:top-[31.5rem] lg:top-[34.5rem] xl:top-[38rem] xl:left-[10rem] lg:left-[6.5rem] sm3:w-1/2 sm3:top-[20rem] sm3:left-[5rem]">
-            I am a passionate and creative  <span className="font-bold text-blue-400">Full Stack Developer</span> with experience in creating highly polished interfaces for the <span className="font-bold text-blue-400">web</span> & mobile.
+            I am a passionate and creative <span className="font-bold text-blue-400">Full Stack Developer</span> with experience in creating highly polished interfaces for the <span className="font-bold text-blue-400">web</span> & mobile.
           </div>
+
           <div className="absolute z-30 flex space-x-3 lg:top-[35rem] xl:top-[38rem] md:right-8 xl:right-[10rem] md:top-[31rem]">
             <a href="https://github.com/zklou" target="_blank" rel="noreferrer">
               <div className="text-white bg-black rounded-full sm1:text-xl md:text-2xl lg:text-3xl sm1:p-3 md:p-3 lg:p-3 hover:bg-white hover:text-black hover:scale-75"><FiGithub /></div>
@@ -131,7 +130,6 @@ const Home = () => {
           </div>
         </div>
 
-
         {/* Mobile Home */}
         <div id="home" className="relative top-0 max-h-screen bg-cover border-0 sm1:block md:hidden">
           <div className="w-full pt-20 m-auto border-0 opacity-1" id="main">
@@ -141,29 +139,15 @@ const Home = () => {
             <motion.div style={{ x: toRight2 }} className="top-[12rem] absolute text-gray-400/50 overflow-hidden whitespace-nowrap font-stencil24 text-6xl font-extrabold">
               web web web web web web web web web web web web web web web web web web web web
             </motion.div>
-            {/* 重复的 motion.div 按相同方式调整 */}
             <div className="relative z-10 w-full m-auto -ml-4 border-0">
               <Image src="./media/images/meformal.png" alt="" width={800} height={100} className="m-auto transition duration-300 transform grayscale brightness-75" />
             </div>
             <div className="absolute z-0 font-extrabold tracking-tighter text-center text-white font-change2 sm1:text-5xl left-3 sm1:top-[14rem] sm2:top-[15rem] sm2:text-6xl sm2:left-0 sm3:left-8">FULLSTACK</div>
-            <div className="absolute z-20 font-extrabold tracking-tighter text-center text-white font-change2 font-stencil2 sm1:text-5xl left-3 sm1:top-[14rem] sm2:top-[15rem] sm2:text-6xl sm2:left-0 sm3:left-8">FULLSTACK</div>
-            <div className="absolute z-20 font-extrabold tracking-tighter text-center font-change2 text-blue-400 sm1:text-6xl sm1:left-3 sm1:top-[17rem] sm2:top-[18rem] sm2:left-0 sm3:left-8">WEB</div>
-            <div className="absolute z-0 text-5xl font-extrabold tracking-tighter text-white font-change2 left-3 sm1:top-[21rem] sm2:text-6xl sm2:left-0 sm3:left-8 sm2:top-[21rem]"><span>DEVELOPER</span></div>
-            <div className="absolute z-20 text-5xl font-extrabold tracking-tighter text-white font-change2 font-stencil2 left-3 sm1:top-[21rem] sm2:text-6xl sm2:left-0 sm3:left-8 sm2:top-[21rem]"><span>DEVELOPER</span></div>
+            <div className="absolute z-20 font-extrabold tracking-tighter text-center text-blue-400 font-change2 sm1:text-5xl left-3 sm1:top-[14rem] sm2:top-[15rem] sm2:text-6xl sm2:left-0 sm3:left-8">WEB DEVELOPER</div>
           </div>
+
           <div className="absolute z-40 w-11/12 text-sm text-gray-300 border-0 top-[28rem] sm3:left-[2rem] sm1:left-[1rem] sm1:hidden">
             Hello, I'm <span className="font-bold text-blue-400">Zhengkun Lou</span>, a passionate and creative <span className="font-bold text-blue-400">Full Stack Developer</span> with experience in creating highly polished interfaces for the <span className="font-bold text-blue-400">web</span> & mobile.
-          </div>
-          <div className="z-50 flex flex-row absolute sm1:top-[24rem] sm2:top-[25rem] md:top-[33rem] space-x-2 justify-end right-4">
-            <a href="https://github.com/zklou" target="_blank" rel="noreferrer">
-              <div className="text-white bg-black rounded-full sm1:text-xl md:text-2xl lg:text-3xl sm1:p-3 md:p-3 lg:p-3 hover:bg-white hover:text-black hover:scale-75"><FiGithub /></div>
-            </a>
-            <a href="https://www.linkedin.com/in/zhengkun-lou/" target="_blank" rel="noreferrer">
-              <div className="text-white bg-black rounded-full sm1:text-xl md:text-2xl lg:text-3xl sm1:p-3 md:p-3 lg:p-3 hover:bg-white hover:text-black hover:scale-75"><FaLinkedinIn /></div>
-            </a>
-            <a href="https://www.instagram.com/havagr8d/" target="_blank" rel="noreferrer">
-              <div className="text-white bg-black rounded-full sm1:text-xl md:text-2xl lg:text-3xl sm1:p-3 md:p-3 lg:p-3 hover:bg-white hover:text-black hover:scale-75"><FiInstagram /></div>
-            </a>
           </div>
         </div>
 
@@ -185,9 +169,9 @@ const Home = () => {
         <div id="contact" className="pt-32 border-0">
           <Contact />
         </div>
-
       </div>
     </>
-  )
+  );
 }
-export default Home
+
+export default Home;
