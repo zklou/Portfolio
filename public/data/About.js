a1cd96cba3a956c0374718e34fd28589cdd685a4
Zhengkun Lou <41825013+zklou@users.noreply.github.com>
2024-10-26 12:28:11 -0400

    Image src ./

--- public/data/About.js
+++ public/data/About.js
@@ -5,7 +5,7 @@ export const scrollData = [
     heading2: 'journey',
     maskText: 'Research & Projects',
     description: "Currently pursuing a Master's in Computer Science at Georgia Tech, after earning a Bachelor's degree from York University. My academic journey has been transformative, fostering a passion for learning that continues to drive me. From classroom theories to real-world applications, education has shaped my approach to problem-solving.",
-    imgUrl: '/media/images/education2.png',
+    imgUrl: './media/images/education2.png',
   },
   {
     id: '2',
@@ -13,7 +13,7 @@ export const scrollData = [
     heading2: 'developer',
     maskText: 'Coding & Cloud :)',
     description: "I'm a dedicated full-stack developer with expertise in React, Spring Cloud, and AWS. I thrive in creating scalable, user-centric web applications. My professional experience at Yongtai Quanwei Decor Ltd. has refined my skills, turning complex business needs into effective technical solutions.",
-    imgUrl: '/media/images/coding2.gif',
+    imgUrl: './media/images/coding2.gif',
   },
   {
     id: '3',
@@ -21,6 +21,6 @@ export const scrollData = [
     heading2: 'the screen',
     maskText: 'Nature, Music & Friends :)',
     description: "When not coding, I find balance in nature and music. I enjoy crafting soundscapes with tools like Max 8, blending creativity with technology. Sci-fi movies and time with friends also offer a refreshing break, helping me recharge before tackling the next challenge.",
-    imgUrl: '/media/images/beyond3.gif',
+    imgUrl: './media/images/beyond3.gif',
   },
 ];
